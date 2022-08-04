d5721a09f4e76375d82c238e3b251103c68f95d3
piesieomane <piesieyeboah@gmail.com>
2022-08-04 23:08:19 +0300

    setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+    </>
+  );
+};
+
+export default App;
